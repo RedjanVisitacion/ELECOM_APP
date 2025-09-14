043edc5b1330f60cdbe2448d997ba4997ddf2985
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-09-14 10:21:54 +0800

    Creating services fonder

---
+++ lib/services/auth.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  // sign in anonymously
+
+  // sign in with email & password
+
+  // register with email & password
+
+  // sign out
+}
